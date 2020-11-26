c53d9f918c1619b639de31cb869975363f9f0362
Paul <paulieracisz@gmail.com>
2020-11-26 13:05:27 -0500

    media queries for mobile and removing all of the bootstrap CSS because it doesn't resize well

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -13,6 +13,7 @@ import B4Thumbnail from '../resources/b4thumbnail.png'
 import GOCThumbnail from '../resources/gocthumbnail.png'
 import MGThumbnail from '../resources/mgthumbnail.png'
 import TOHThumbnail from '../resources/tohthumbnail.png'
+import Skills from '../resources/Skills.png'
 import '../stylesheets/work.css'
 
 
@@ -23,13 +24,14 @@ export default function HomePage() {
                 <NavLinks />
                 <h2 id="Iam">I am Paul Racisz.</h2>
                 <div id="big-p-box">
-                    <div id="profile-card" class="card" style={{ width: "15em" }}>
-                        <img class="card-img-top" src={Me} alt="Card cap" />
-                        <div  class="card-body" id="profile-body">
-                            <h5 class="card-title">Paul Racisz</h5>
-                            <p class="card-text">Paul is an avid video game nerd, hobbyist weight lifter, and passionate coder. His technical experience is vast, but he specializes in React, and Javascript.
+                    <div>
+                        <img id="me-pic" src={Me} alt="Card cap" />
+                        <div id="profile-box">
+                            <h5 id="paul-title">Paul Racisz</h5>
+                            <p id="paul-desc">Paul is an avid video game nerd, 
+                            <br/> hobbyist weight lifter, and passionate coder. <br/>His technical experience is vast, but he specializes in React,<br/> and Javascript.
                             <br/><br/>Phone Number: <br/>(734)- 624 - 9048<br/> Email: paulieracisz@gmail.com <br/> </p>
-                                <div>
+                                <div id="social-links">
                                     <a href="https://github.com/Paulracisz" target="_blank" rel="noreferrer">
                                     <img className="logo" src={GitHubLogo} alt="github logo" style={{width: "2em", height: "2em"}}/>
                                     </a>
@@ -42,100 +44,83 @@ export default function HomePage() {
                                 </div>
                                 </div>
                             </div>
-                            <div class="card" style={{ width: "30em" }}>
-                                <div class="card-body">
-                                    <h5 id="cl-title" class="card-title">Cover Letter</h5>
-                                    <p class="card-text">
-As an early career software engineering professional with some light technical experience, I am an ideal candidate for the Software Engineer position available with your company.  Please accept <a href="/resume">my resume</a> for your consideration.
-I am a motivated professional with a demonstrable record of helping organizations meet their goals.  My positive attitude, world-class work ethic, and attention to detail have helped me succeed in a variety of fast-paced environments.  I take my work seriously, and approach every undertaking with enthusiasm, diligence, and positivity. I am ready to tackle a challenging, professional position that makes excellent use of my multi-tasking, project coordination, engineering, organizational, and relationship management skills.  
-My strong communication skills and years of experience working with diverse groups allow me to move with ease and obtain commitments among clients and coworkers of varied backgrounds and different levels of responsibility.  I have the utmost respect for confidentiality, and approach sensitive matters with discretion and tact.  I am especially skilled at keeping operations running smoothly and efficiently in the midst of changes to technologies, procedures, or personnel. 
-My knowledge of the full software engineering & development life cycle experience have prepared me well for a challenging role with your company.  I look forward to meeting with you to discuss how I can help your organization meet its objectives in 2020 and beyond.  Please don’t hesitate to contact me with any questions.  Thanks so much for your time and consideration. <br/> Sincerely, <br/>
-<p style={{fontFamily: "Gabriola", fontSize: "2.5em"}}>Paul Racisz</p>
+                            <div>
+                                <div id="cv-box">
+                                    <h5 id="cv-title">Cover Letter</h5>
+                                    <p id="cv-text">
+As an early career software engineering professional with some light technical experience, I am an ideal candidate for the Software Engineer <br/>position available with your company. Please accept <a href="#resume">my resume</a> for your consideration.<br/>
+I am a motivated professional with a demonstrable record of helping organizations meet their goals.<br/>  My positive attitude, world-class work ethic, and attention to detail have helped me succeed in a variety of fast-paced environments.<br/>  I take my work seriously, and approach every undertaking with enthusiasm, diligence, and positivity.<br/> I am ready to tackle a challenging, professional position that makes excellent use of my multi-tasking, project coordination, engineering, organizational,<br/> and relationship management skills.  
+My strong communication skills and years of experience working with diverse groups allow me<br/> to move with ease and obtain commitments among clients and coworkers of varied backgrounds and different levels of responsibility.<br/>  I have the utmost respect for confidentiality, and approach sensitive matters with discretion and tact.<br/>  I am especially skilled at keeping operations running smoothly and efficiently in the midst of changes to technologies, procedures, or personnel. <br/>
+My knowledge of the full software engineering & development life cycle experience have prepared me well for a challenging role with your company.<br/>  I look forward to meeting with you to discuss how I can help your organization meet its objectives in 2020 and beyond.<br/>  Please don’t hesitate to contact me with any questions.<br/>  Thanks so much for your time and consideration. <br/> Sincerely, <br/>
+<p style={{fontFamily: "Gabriola", fontSize: "2.5em", textAlign: "center",}}>Paul Racisz</p>
 
 </p>
                         </div>
                     </div>
-                </div>
-                <div id="progress-card" class="card" style={{ width: "45em" }}>
-                <div  class="card-body" id="profile-body">
-                <h5 id="cl-title" class="card-title">Skills</h5>
-<div class="progress">
-  <div class="progress-bar bg-danger" role="progressbar" style={{width: "60%"}} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">Django Skill</div>
-</div>
-<div class="progress">
-  <div class="progress-bar bg-danger" role="progressbar" style={{width: "65%"}} aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">Python Skill</div>
-</div>
-                <div class="progress">
-  <div class="progress-bar bg-info" role="progressbar" style={{width: "85%"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Javascript Skill</div>
-</div>
-<div class="progress">
-  <div class="progress-bar bg-info" role="progressbar" style={{width: "85%"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">React Skill</div>
-</div>
-<div class="progress">
-  <div class="progress-bar bg-info" role="progressbar" style={{width: "95%"}} aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">CSS Skill</div>
-</div>
             </div>
+                    <div>
+                    <img  id="skills-img" src={Skills} alt="skills bar graph"></img>
             </div>
             <div id="big">
-                <NavLinks />
-                <h2>Resume Preview</h2>
+                <h2 id="resume">Resume Preview</h2>
                 <a href={ResumeFile} download>Click here to download the full resume.</a>
-                <div class="card">
-                    <div class="card-body">
-                    <img src={Resume} alt="resume" />
+                <div class="resume-box">
+                    <div class="resume-body">
+                    <img id="resume-img" src={Resume} alt="resume" />
                     </div>
                 </div>
             </div>
             <h2>Work</h2>
-                <p>Click on the Images of the projects to go to their deployed site.</p>
-                <div id="card-box">
-                    <div class="card" style={{ width: "15em" }}>
+                <p id="click-text">Click on the Images of the projects to go to their deployed site.</p>
+                <div id="work-box">
+                    <div class="card-container" style={{ width: "15em" }}>
                     <a href="https://paulracisz.gitlab.io/connect-four/" target="_blank" rel="noreferrer">
-                        <img id="b4-card" class="card-img-top" src={B4Thumbnail} alt="Card cap" />
+                        <img class="card-img" src={B4Thumbnail} alt="Card cap" />
                         </a>
-                        <div class="card-body">
-                            <h5 class="card-title">Blink 4 Web Game</h5>
-                            <p class="card-text">A local multiplayer Connect-4 mock web game, developed using only javascript, with stunning UI, and beautiful visuals.</p>
+                        <div class="box-card">
+                            <h5 id="title">Blink 4 Web Game</h5>
+                            <p class="text">A local multiplayer Connect-4 mock web game, developed using only javascript, with stunning UI, and beautiful visuals.</p>
                         </div>
                     </div>
-                    <div class="card" style={{ width: "15em" }}>
+                    <div class="card-container" style={{ width: "15em" }}>
                         <a href="https://paulracisz.gitlab.io/photos-from-here" target="_blank" rel="noreferrer">
-                        <img id="pfh-card" class="card-img-top" src={PFHThumbnail} alt="Card cap"/>
+                        <img class="card-img" src={PFHThumbnail} alt="Card cap"/>
                         </a>
-                        <div class="card-body">
-                            <h5 class="card-title">Photos From Here</h5>
-                            <p class="card-text">Photos From Here utilizes the Geolocation API to parse the users Coordinates, and then communicates with the Flickr API, to serve up photos that were taken near them.</p>
+                        <div class="box-card">
+                            <h5 class="title">Photos From Here</h5>
+                            <p class="text">Photos From Here utilizes the Geolocation API to parse the users Coordinates, and then communicates with the Flickr API, to serve up photos that were taken near them.</p>
                         </div>
                     </div>
-                    <div class="card" style={{ width: "15em" }}>
+                    <div class="card-container" style={{ width: "15em" }}>
                     <a href="https://paulracisz.gitlab.io/game-of-chance" target="_blank" rel="noreferrer">
-                        <img id="b4-card" class="card-img-top" src={GOCThumbnail} alt="Card cap" />
+                        <img class="card-img" src={GOCThumbnail} alt="Card cap" />
                         </a>
-                        <div class="card-body">
-                            <h5 class="card-title">Game Of Chance</h5>
-                            <p class="card-text">Game of Chance is a dungeon themed rock paper scissors game that is played against an AI opponent, you must fight an evil wizard to escape the dungeon and win your freedom.</p>
+                        <div class="box-card">
+                            <h5 class="title">Game Of Chance</h5>
+                            <p class="text">Game of Chance is a dungeon themed rock paper scissors game that is played against an AI opponent, you must fight an evil wizard to escape the dungeon and win your freedom.</p>
                         </div>
                     </div>
-                    <div class="card" style={{ width: "15em" }}>
+                    <div class="card-container" style={{ width: "15em" }}>
                     <a href="https://paulracisz.gitlab.io/mazeproject" target="_blank" rel="noreferrer">
-                        <img id="b4-card" class="card-img-top" src={MGThumbnail} alt="Card cap" />
+                        <img class="card-img" src={MGThumbnail} alt="Card cap" />
                         </a>
-                        <div class="card-body">
-                            <h5 class="card-title">Maze Project</h5>
-                            <p class="card-text">Maze Game is a simple, adorable and cheeky game that uses keyboard inputs to guide a dog through a maze to find his bone.</p>
+                        <div class="box-card">
+                            <h5 class="title">Maze Project</h5>
+                            <p class="text">Maze Game is a simple, adorable and cheeky game that uses keyboard inputs to guide a dog through a maze to find his bone.</p>
                         </div>
                     </div>
-                    <div class="card" style={{ width: "15em" }}>
+                    <div class="card-container" style={{ width: "15em" }}>
                     <a href="https://paulracisz.gitlab.io/tower-of-hanoi/" target="_blank" rel="noreferrer">
-                        <img id="b4-card" class="card-img-top" src={TOHThumbnail} alt="Card cap" />
+                        <img class="card-img" src={TOHThumbnail} alt="Card cap" />
                         </a>
-                        <div class="card-body">
-                            <h5 class="card-title">Tower Of Hanoi</h5>
-                            <p class="card-text">The Tower Of Hanoi is a puzzle in which you must transfer several different sized discs from one tower to another, without placing any larger discs on top of smaller ones. I turned this puzzle into a javascript web game.</p>
+                        <div class="box-card">
+                            <h5 class="title">Tower Of Hanoi</h5>
+                            <p class="text">The Tower Of Hanoi is a puzzle in which you must transfer several different sized discs from one tower to another, without placing any larger discs on top of smaller ones. I turned this puzzle into a javascript web game.</p>
                         </div>
                     </div>
                 </div>
             </div>
+            <p id="bottom"></p>
         </>
     )
 }
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -4,9 +4,9 @@ export default function NavLinks() {
     return (
         <>
             <nav class="navbar navbar-light bg-light">
-                <a class="nav-link active" href="/">Home</a>
-                <a class="nav-link active" href="/resume">Resume</a>
-                <a class="nav-link active" href="/work">Work</a>
+                <a class="nav-link active" href="#resume">Resume</a>
+                <a class="nav-link active" href="#work-box">Work</a>
+                <a class="nav-link active" href="#bottom">Bottom</a>
             </nav>
         </>
     )
