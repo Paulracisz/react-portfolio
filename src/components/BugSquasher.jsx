3769e0787e540103bb74993df27f33f27c1ab89f
Paul Racisz <paulieracisz@gmail.com>
2023-02-19 15:06:47 -0500

    you can now squash the bugs, but there are bugs (haha) that need to be fixed with it

--- src/components/BugSquasher.jsx
+++ src/components/BugSquasher.jsx
@@ -1,114 +1,122 @@
-import "../stylesheets/bugsquasher.css"
-import gsap from "gsap"
-import ScrollTrigger from "gsap/ScrollTrigger"
-import { useEffect, useRef } from "react"
-import { useState } from "react"
-import useWindowDimensions from "../helpers/hooks/useWindowDimensions"
+import "../stylesheets/bugsquasher.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
+import { useState } from "react";
+import useWindowDimensions from "../helpers/hooks/useWindowDimensions";
 // import { bugArray } from "../helpers/bugGenerator.js"
-import Bug from '../resources/svgs/bug-svgrepo-com.svg'
-import BugSVG from "../helpers/BugSVG"
+import Bug from "../resources/svgs/bug-svgrepo-com.svg";
+import Splatter from "../resources/svgs/splatter.svg";
+import BugSVG from "../helpers/BugSVG";
 
-export default function BugSquasher(){ 
-    gsap.registerPlugin(ScrollTrigger)
-    const [ bugArray, setBugArray ] = useState([])
-    const { height, width } = useWindowDimensions()
+export default function BugSquasher() {
+  gsap.registerPlugin(ScrollTrigger);
+  const [bugArray, setBugArray] = useState([]);
+  const { height, width } = useWindowDimensions();
 
-    const bugRef = useRef()
-    const tempArray = []
+  const bugRef = useRef();
+  const tempArray = [];
 
-    
+  function generateBugs() {
+    gsap.set(".bug", {
+      // x: getRandomNumber(width, 100),
+      // y: getRandomNumber(height, 100),
+      height: gsap.utils.random(150, 250),
+      width: gsap.utils.random(150, 250),
+    });
+    const randomTop = getRandomNumber(height, 100);
+    const randomLeft = getRandomNumber(width, 100);
+    tempArray.push(<BugSVG bottom={"-50px"} left={randomLeft} />);
+    setBugArray(tempArray);
+    return <BugSVG />;
+  }
 
+  function getRandomNumber(max, min) {
+    return Math.random() * (max - min + min);
+  }
 
-    function generateBugs(){ 
-        gsap.set(".bug", { 
-            // x: getRandomNumber(width, 100),
-            // y: getRandomNumber(height, 100),
-            height: gsap.utils.random(150, 250),
-            width: gsap.utils.random(150, 250)
-        })
-        const randomTop = getRandomNumber(height, 100)
-        const randomLeft = getRandomNumber(width, 100)
-        tempArray.push(<BugSVG bottom = {"-50px"} left = {randomLeft} />) 
-        setBugArray(tempArray)
-        return <BugSVG />
+  function removeBug(bugArray) {}
+
+  useEffect(() => {
+    for (let i = 0; i <= 20; i++) {
+      generateBugs();
     }
-    
-   
+  }, []);
 
-    function getRandomNumber(max, min){
-        return Math.random() * ((max - min) + min)
-    }
+  useEffect(() => {
+    gsap.to(".bug-box", {
+      scrollTrigger: {
+        start: "top 100%",
+        trigger: ".bug-container",
+        end: "top 0%",
+        scrub: true,
+      },
+      transform: "scale3d(1,1,1)",
+      onComplete: () => {
+        gsap.to(".bug-box", {
+          position: "sticky",
+          // position: '-webkit-sticky',
+          top: "0",
+        });
+      },
+    });
+    gsap.to("#squash", {
+      scrollTrigger: {
+        trigger: "#squash",
+        start: "top top",
+        end: "+=1800",
+        scrub: true,
+      },
+      opacity: 1,
+    });
+  }, []);
 
-    function removeBug(bugArray){ 
+  //     gsap.fromTo(".bug", {
+  //         y: 100,
+  //         // left: getRandomNumber(width, 100)
+  //     }, {
+  //         y: -height - 100,
+  //         repeat: -1,
+  //         repeatRefresh: true,
+  //         duration: Math.random() + 1 * 6,
+  //         delay: 'random(0,4)'
+  //     })
+  // }, [bugArray])
 
-    }
+  function squashBug(e, index) {
+    const bugElement = e.target;
+    const bugPos = bugElement.getBoundingClientRect();
+    const updatedBugArray = [...bugArray];
+    updatedBugArray.splice(
+      index,
+      1,
+      <img
+        id="splatter"
+        src={Splatter}
+        alt="splatter"
+        style={{ top: bugPos.top, left: bugPos.left }}
+      />
+    );
+    setBugArray(updatedBugArray);
+  }
 
-    useEffect(() => { 
-        for(let i = 0; i <= 20; i++){ 
-            generateBugs()
-        }
-    }, [])
-    
-    useEffect(() => { 
-        gsap.to(".bug-box", { 
-            scrollTrigger: { 
-                start: "top 100%", 
-                trigger: ".bug-container",
-                end: "top 0%", 
-                scrub: true
-            },
-            transform: "scale3d(1,1,1)",
-            onComplete: () => { 
-                gsap.to('.bug-box', { 
-                    position: "sticky",
-                    // position: '-webkit-sticky',
-                    top: "0"
-                })
-            }
-        })
-        gsap.to("#squash", { 
-            scrollTrigger: { 
-                trigger:"#squash",
-                start: "top top", 
-                end: "+=1800",
-                scrub: true
-            },
-            opacity: 1
-        })
-    },[ ])
-
-
-
-    //     gsap.fromTo(".bug", { 
-    //         y: 100,
-    //         // left: getRandomNumber(width, 100) 
-    //     }, { 
-    //         y: -height - 100,
-    //         repeat: -1,
-    //         repeatRefresh: true,
-    //         duration: Math.random() + 1 * 6,
-    //         delay: 'random(0,4)'
-    //     })
-    // }, [bugArray])
-    
-    function squashBug(e){ 
-        
-    }
-
-    return( 
-        <div className = "bug-container">
-            <div className="bug-box">
-                {bugArray.map((bug, index) => { 
-                    return (
-                        <div onClick = {(e) => squashBug(e)} ref = {bugRef} index = {index} > 
-                            {bug}
-                        </div>
-                    )
-                })}
-                {/* <BugSVG /> */}
-                    <div id="squash">I squash bugs.</div>
-
+  return (
+    <div className="bug-container">
+      <div className="bug-box">
+        {bugArray.map((bug, index) => {
+          return (
+            <div
+              onClick={(e) => squashBug(e, index)}
+              ref={bugRef}
+              index={index}
+            >
+              {bug}
             </div>
-        </div>
-    )
+          );
+        })}
+        {/* <BugSVG /> */}
+        <div id="squash">I squash bugs.</div>
+      </div>
+    </div>
+  );
 }
