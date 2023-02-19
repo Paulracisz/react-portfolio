06e62681fd24949ec9d362221b3f00634c041f30
Paul Racisz <paulieracisz@gmail.com>
2023-02-19 14:04:44 -0500

    contact form now clears after submission, and formatting

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,40 +1,59 @@
-import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
-import '../stylesheets/homepage.css'
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+import "../stylesheets/homepage.css";
 
 export const ContactUs = () => {
   const form = useRef();
+  const formUserName = document.getElementById("usernameinput");
+  const formEmail = document.getElementById("emailinput");
+  const formText = document.getElementById("textinput");
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('service_9cnan4m', 'template_cau5y88', form.current, 'user_8H2okSIPMyJBrfkGMvZv4')
-      .then((result) => {
-          window.alert("Message Sent Successfully.")
+    formUserName.value = "";
+    formEmail.value = "";
+    formText.value = "";
+    emailjs
+      .sendForm(
+        "service_9cnan4m",
+        "template_cau5y88",
+        form.current,
+        "user_8H2okSIPMyJBrfkGMvZv4"
+      )
+      .then(
+        (result) => {
+          window.alert("Message Sent Successfully.");
           console.log(result.text);
-
-      }, (error) => {
-        window.alert("An error has occured, please try again later." + error.text)
+        },
+        (error) => {
+          window.alert(
+            "An error has occured, please try again later." + error.text
+          );
           console.log(error.text);
-      });
+        }
+      );
   };
 
   return (
-    <form ref={form} id="contactformbox"onSubmit={sendEmail}>
-      <label className="text">Name
-      <br/>
-      <input type="text" id="usernameinput" name="user_name" />
+    <form ref={form} id="contactformbox" onSubmit={sendEmail}>
+      <label className="text">
+        Name
+        <br />
+        <input type="text" id="usernameinput" name="user_name" />
       </label>
-      <label className="email">Email
-      <br/>
-      <input type="email" name="user_email" />
+      <label className="email">
+        Email
+        <br />
+        <input type="email" id="emailinput" name="user_email" />
       </label>
-      <label className="message">Message
-      <br/>
-      <textarea name="message" />
+      <label className="message">
+        Message
+        <br />
+        <textarea id="textinput" name="message" />
       </label>
-      <label className="submit">Submit
-      <br/>
-      <input className="submit" type="submit" value="Send" />
+      <label className="submit">
+        Submit
+        <br />
+        <input className="submit" type="submit" value="Send" />
       </label>
     </form>
   );
