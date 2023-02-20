1bfff859ccd730bb74f510eafa48c752b6dff6b1
Paul Racisz <paulieracisz@gmail.com>
2023-02-19 20:48:54 -0500

    removed all unused assets

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import {
 } from 'react-router-dom';
 import HomePage from "./components/HomePage.jsx"
 import FourZeroFour from "./components/FourZeroFour.jsx"
-import Work from "./components/Work.jsx"
-import Resume from "./components/Resume.jsx"
 import BugSquasher from './components/BugSquasher.jsx';
 
 
@@ -19,8 +17,6 @@ function App() {
     <Router>
         <Routes>
           <Route path="/" element = {<HomePage /> }></Route>
-          <Route path="/work" element = {<Work/>}></Route>
-          <Route path="/resume" element = {<Resume/>}></Route>
           <Route path = "/test" element = {<BugSquasher />}></Route>
           <Route path = "*" element={<FourZeroFour />} />
         </Routes>
